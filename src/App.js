5d7c89f869a852ebfe1093aba9c1a4e673f054aa
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-07-24 12:26:36 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinDetailPage from "./pages/CoinDetailPage";
+import CoinSummaryPage from "./pages/CoinSummaryPage";
+import Header from "./components/Header";
+import "./App.css";
+import { WatchListContextProvider } from "./context/watchListContext";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={CoinSummaryPage} />
+          <Route path="/coins/:id" component={CoinDetailPage} />
+        </BrowserRouter>
+      </WatchListContextProvider>
     </div>
   );
-}
+};
 
 export default App;
+
